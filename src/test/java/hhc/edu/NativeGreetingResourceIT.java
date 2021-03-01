c8475833c19b9769ef45ecd5fc99345ec521c486
hector.hernandez <hector.hernandez@conceptomovil.com>
2021-03-01 16:32:55 -0600

    add example quarkus and maven

---
+++ src/test/java/hhc/edu/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package hhc.edu;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
